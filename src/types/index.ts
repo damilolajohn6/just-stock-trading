f934e164cefaab88fe1742f196989dcaa80ae463
Damilola john <97457114+damilolajohn6@users.noreply.github.com>
2026-02-16 10:20:38 +0100

    types

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,6 +3,5 @@ export * from './product';
 export * from './cart';
 export * from './order';
 export * from './user';
-export * from './category';
 export * from './payment';
 export * from './api';
